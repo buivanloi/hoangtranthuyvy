404e81cffc9da6156e2eec159456546a68e8e289
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-04 22:46:43 +0700

    Add files via upload
    hihi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/khanhgiang.duongnguyen.12/photos_by';
+const eventName = 'Bùi Lợi chúc mừng ngày 8/3';
+const titleCard = 'Tặng GIang';
+const contentCard = 'Từ trước đên nay chưa từng thấy Giang rơi nước mắt, và cũng không hy vọng có ngày đấy. Nhân ngày 8-3, chúc Giang mãi giữ nụ cười trên môi và sớm gặp được người khiến em hạnh phúc. nhé!  ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'khanh-giang.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
